669a3ba99c700eaf4ce1c95f97b54010703564f0
erick <erick261@gmail.com>
2022-06-14 01:46:21 +0200

    change addcomment controller

--- controllers/controladores.js
+++ controllers/controladores.js
@@ -5,19 +5,15 @@ const db = require('../db')
 let updateComment;
 
 const addComment = async (req, res) => {
-    const {username, comments} = req.body
+    const {comments, id} = req.body
+    console.log(req.body)
     try{
-    if (!username || !comments) {
+    if (!id || !comments) {
         res.status(403).send("[ERROR] Faltan datos para añadir un comentario")
         return
     }
-    const selectUserId = `select userId from services inner join users on users.id = services.userId where username ='${username}'`
     const connection = await db.getConnection()
-    const userId = await connection.query(selectUserId)
-    
-    const user2 =userId[0][0].userId
-   
-    updateComment = `UPDATE services SET comments= "${comments}" where userId=${user2}`
+    updateComment = `UPDATE services SET comments= "${comments}" where id=${id}`
     
     await connection.query(updateComment)
     res.status(200).send("[EXITO] Comentario añadido correctamente")
